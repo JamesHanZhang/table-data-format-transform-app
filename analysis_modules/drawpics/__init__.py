f42aec85611bed71ebddb272516a28b2f6a8a331
JamesHanZhang <jameszhang.workspace@outlook.com>
2023-11-07 22:26:37 +0800

    [DELETE] 去掉多余功能, 专注格式转换

--- analysis_modules/drawpics/__init__.py
+++
@@ -1,5 +0,0 @@
-
-# 包内模块导入外包
-
-# 将包内模块暴露在外部
-from analysis_modules.drawpics.bar_chart_creation import BarChartCreation
--- main/wrapped_main/wrap_exe_cmd.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# 打包csv转excel快速文件
-# 注意需要先将default_properties里面的path_properties的参数都空置为""
-
-# 快速将csv转为excel
-pyinstaller --onefile --icon=exe_icon.ico csv_to_excel_fast_transfer.py

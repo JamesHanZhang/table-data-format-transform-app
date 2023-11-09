fcd8d485857d2e7239560e23f68eb8ebcd0dcd4a
JamesHanZhang <jameszhang.workspace@outlook.com>
2023-11-09 17:50:44 +0800

    [UPDATE] 新增对DATETIME的合理的转换及导出功能

--- analysis_modules/sql_output_drivers/oracle_output_driver.py
+++ analysis_modules/sql_output_drivers/oracle_output_driver.py
@@ -1,4 +1,3 @@
-
 # self-made modules
 import analysis_modules.default_properties as prop
 from analysis_modules.sql_output_drivers.sql_output_driver import SqlOutputDriver
@@ -6,7 +5,7 @@ from analysis_modules.params_monitor import OutputParams
 
 
 class OracleOutputDriver(SqlOutputDriver):
-    def __init__(self, output_params: OutputParams, params_set: str=prop.DEFAULT_PARAMS_SET):
+    def __init__(self, output_params: OutputParams, params_set: str = prop.DEFAULT_PARAMS_SET):
         super().__init__(output_params, params_set)
         
         # 不同数据库的从pandas的类型到数据库的类型的转换
@@ -30,9 +29,15 @@ class OracleOutputDriver(SqlOutputDriver):
         需要依据数据库的不同，重写的函数
         如果元素为datetime64的时候，元素在sql里应该怎么写
         """
-        if col_type == 'datetime64':
+        if 'datetime' in col_type:
             # 这里可能会因为实际需要需时常改动
-            element = f"TO_DATE('{element}', 'yyyy-mm-dd hh24:mi:ss')"
+            try:
+                date_format = self.to_date_formats[col]
+            except KeyError:
+                date_format = 'yyyy-mm-dd hh24:mi:ss'
+            if date_format == "":
+                date_format = 'yyyy-mm-dd hh24:mi:ss'
+            element = f"TO_DATE('{element}', '{date_format}')"
         elif col_type == 'timedelta64':
             element = f"TO_DSINTERVAL('{element}')"
         return element
@@ -44,20 +49,20 @@ class OracleOutputDriver(SqlOutputDriver):
         """
         commit_line = "COMMIT;\n"
         return commit_line
-        
     
-    def construct_creation_cmd(self, columns:list[str],  dtypes: dict[str, type]) -> str:
+    def construct_creation_cmd(self, columns: list[str], dtypes: dict[str, type]) -> str:
         """
         需要依据数据库的不同，重写的函数
         根据table_name, table_structure, 各列类型进行重写
         :return: 构建建表语句的内容
         """
-        remark_note =f"-- TABLE {self.table_name}: table creation sql command based on database {self.database}\n\n"
+        remark_note = f"-- TABLE {self.table_name}: table creation sql command based on database {self.database}\n\n"
         
         line_construct = list()
         for col in columns:
             if str(dtypes[col]) in self.var_list:
-                line_construct.append(f"    {col} {self.db_types_format[str(dtypes[col])]}({str(self.table_structure[col])})")
+                line_construct.append(
+                    f"    {col} {self.db_types_format[str(dtypes[col])]}({str(self.table_structure[col])})")
             else:
                 line_construct.append(f"    {col} {self.db_types_format[str(dtypes[col])]}")
         line_construct = ',\n'.join(line_construct)

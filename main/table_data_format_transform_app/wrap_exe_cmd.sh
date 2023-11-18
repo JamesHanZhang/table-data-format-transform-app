ab77aad907f11bda03b927fdcf97d9e2c5f1e96a
JamesHanZhang <jameszhang.workspace@outlook.com>
2023-11-18 19:24:58 +0800

    [UPDATE] 修改了代码，所以更新release

--- main/table_data_format_transform_app/wrap_exe_cmd.sh
+++ main/table_data_format_transform_app/wrap_exe_cmd.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 # 只有通过外部路径的执行, 才可以保证该脚本不被自动打包仅exe内, 可以作为依赖文件放置在外面
+# 先修改basic_process_params.py, import_params.py, output_params.py的脚本, 使得三个参数表外置, 然后再正常进行打包
 # 因为tabulate在打包环境无法被打包，所以手动标注打包 --hidden-import=tabulate
 pyinstaller --name=table-data-format-transform-app --onefile --hidden-import=tabulate --icon=rabbit_exe_icon1.ico table_data_format_transform_app.py
